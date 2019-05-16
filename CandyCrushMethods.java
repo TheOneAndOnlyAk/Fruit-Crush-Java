61be10e6406bf4284df5563575989ec25e0de6e2
TheOneAndOnlyAk <43016555+TheOneAndOnlyAk@users.noreply.github.com>
2019-05-16 13:12:21 -0400

    Add files via upload

--- CandyCrushMethods.java
+++ CandyCrushMethods.java
@@ -1,5 +1,13 @@
 package finalProject;
 
-public interface CandyCrushMethods {
+public interface CandyCrushMethods 
+{
+	public int[][] intitalize(int[][] board);
+	
+	public int[][] replace(int[][] board, int column);
+	
+	public void check(int[][] board);
+	
+
 
 }
